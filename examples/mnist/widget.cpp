db79cf6f8a222db416ad8c86d8c1d61ce61ff9c9
eidelen <adrian.schneider.pr@gmail.com>
2017-06-01 17:41:54 +0200

    Load MNIST dataset

--- examples/mnist/widget.cpp
+++ examples/mnist/widget.cpp
@@ -1,11 +1,15 @@
 #include "widget.h"
 #include "ui_widget.h"
 
+#include "mnist/mnist_reader.hpp"
+
 #include <QFile>
+#include <vector>
 
 Widget::Widget(QWidget* parent) : QMainWindow(parent), ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    mnist::read_dataset<std::vector, std::vector, uint8_t, uint8_t>();
 }
 
 Widget::~Widget()
